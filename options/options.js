3f41c6d2addd121027f1e1a0883ac252bdae7712
Crowdbotics <team@crowdbotics.com>
2023-10-30 20:25:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hobix-44227",
+  url: "https://hobix-44227.botics.co",
+  api: "https://hobix-44227.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
